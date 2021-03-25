d0b8b4d314a98107672e72ff4f6ad75f43f2f747
James A Sutherland <j@sutherland.pw>
2021-03-25 17:08:16 -0500

    Implement result checking, clean up server socket after use, document CLI options in --help

--- PacsTest/Program.cs
+++ PacsTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -12,34 +13,37 @@ namespace PacsTest
     class Options
     {
         [Option('g',"goodid",Required = true,HelpText = "Patient ID of patient with data present")]
-        internal string GoodID { get; set; }
+        public string GoodID { get; set; }
 
         [Option('e',"emptyid",Required = true,HelpText = "Patient ID of patient with NO data present")]
-        internal string EmptyID { get; set; }
+        public string EmptyID { get; set; }
 
         [Option('b',"badid",Required = true,HelpText = "Invalid Patient ID")]
-        internal string BadID { get; set; }
+        public string BadID { get; set; }
 
-        [Option('d',"daterange",Required = true,HelpText = "Date range to search for in DICOM format ()")]
-        internal string DateRange { get; set; }
+        [Option('d',"daterange",Required = true,HelpText = "Date range to search for in DICOM format (yyyymmdd-yyyymmdd)")]
+        public string DateRange { get; set; }
 
         [Option('h',"host",Required = true,HelpText = "Hostname or IP of PACS to test against")]
-        internal string PacsHost { get; set; }
+        public string PacsHost { get; set; }
 
         [Option('p',"port",Required = false,HelpText = "Port number of PACS to test against",Default = 104)]
-        internal int PacsPort { get; set; }
+        public int PacsPort { get; set; }
 
         [Option('o',"port",Required = false,HelpText = "Port number to listen on",Default = 104)]
-        internal int SelfPort { get; set; }
+        public int SelfPort { get; set; }
 
         [Option('n',"pacsname",Required = true,HelpText = "AET of PACS to test against")]
-        internal string PacsName { get; set; }
+        public string PacsName { get; set; }
 
         [Option('s',"selfname",Required = true,HelpText = "Port number of PACS to test against")]
-        internal string SelfName { get; set; }
+        public string SelfName { get; set; }
 
         [Option('m',"movename",Required = true,HelpText = "Name of PACS to send data to")]
         public string MoveName { get; set; }
+        
+        [Option('v',"verbose",HelpText="Show verbose progress information")]
+        public bool Verbose { get; set; }
     }
 
     class ProcResult
@@ -47,8 +51,10 @@ namespace PacsTest
         readonly public int Exitcode;
         readonly public string Stdout, Stderr;
 
-        internal ProcResult(string cmd, string args)
+        internal ProcResult(string cmd, string args,bool verbose)
         {
+            if (verbose)
+                Console.WriteLine($"Running command '{cmd}' with args '{args}'");
             using var p = new Process
             {
                 StartInfo = new ProcessStartInfo
@@ -57,7 +63,8 @@ namespace PacsTest
                     FileName = cmd,
                     Arguments = args,
                     CreateNoWindow = true,
-                    RedirectStandardOutput = true
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
                 }
             };
             p.Start();
@@ -65,10 +72,12 @@ namespace PacsTest
             p.BeginErrorReadLine();
             var outbuff=new StringBuilder();
             var errbuff=new StringBuilder();
-            p.OutputDataReceived += (p, l) => outbuff.Append(l.Data);
-            p.ErrorDataReceived += (p, l) => errbuff.Append(l.Data);
+            p.OutputDataReceived += (p, l) => outbuff.AppendLine(l.Data);
+            p.ErrorDataReceived += (p, l) => errbuff.AppendLine(l.Data);
             p.WaitForExit();
             Exitcode = p.ExitCode;
+            if (verbose)
+                Console.WriteLine($"'{cmd} {args}' exited with code {Exitcode}");
             Stdout = outbuff.ToString();
             Stderr = errbuff.ToString();
         }
@@ -76,41 +85,48 @@ namespace PacsTest
     
     class Program
     {
-        static void BreakerSocket(int port, int bytes, string farip, int farport)
+        static void BreakerSocket(int port, int bytes, string farip, int farport, bool verbose)
         {
+            if (verbose)
+                Console.WriteLine($"Creating breaker socket listening on port {port} relaying to {farip}:{farport} for {bytes} bytes");
             var server = new TcpListener(IPAddress.Any,port);
             server.Start();
-            var client = server.AcceptTcpClient();
-            var remote = new TcpClient(farip, farport);
             Task.Run(() =>
             {
-                int b;
-                var i = client.GetStream();
-                var o = remote.GetStream();
-                while ((b = i.ReadByte()) != -1 && bytes-->0)
+                var client = server.AcceptTcpClient();
+                var remote = new TcpClient(farip, farport);
+                Task.Run(() =>
                 {
-                    o.WriteByte((byte) b);
-                }
-                client.Close();
-                remote.Close();
-            });
-            Task.Run(() =>
-            {
-                int b;
-                var i = remote.GetStream();
-                var o = client.GetStream();
-                while ((b = i.ReadByte()) != -1 && bytes-->0)
+                    int b;
+                    var i = client.GetStream();
+                    var o = remote.GetStream();
+                    while ((b = i.ReadByte()) != -1 && bytes-->0)
+                    {
+                        o.WriteByte((byte) b);
+                    }
+                    client.Close();
+                    remote.Close();
+                    server.Stop();
+                });
+                Task.Run(() =>
                 {
-                    o.WriteByte((byte) b);
-                }
-                client.Close();
-                remote.Close();
+                    int b;
+                    var i = remote.GetStream();
+                    var o = client.GetStream();
+                    while ((b = i.ReadByte()) != -1 && bytes-->0)
+                    {
+                        o.WriteByte((byte) b);
+                    }
+                    client.Close();
+                    remote.Close();
+                    server.Stop();
+                });
             });
         }
         
         static bool Test1(Options o)
         {
-            var p=new ProcResult("echoscu", $"{o.PacsHost} {o.PacsPort.ToString()}");
+            var p=new ProcResult("echoscu", $"{o.PacsHost} {o.PacsPort.ToString()}", o.Verbose);
             return p.Exitcode == 0;
         }
 
@@ -136,9 +152,9 @@ namespace PacsTest
             if (n == 7)
             {
                 // Cut off outbound connection mid-stream
-                pacsport = (o.SelfPort + 1).ToString();
+                pacsport = (o.PacsPort + 1).ToString();
                 pacshost = "127.0.0.1";
-                BreakerSocket(o.SelfPort+1,1024,o.PacsHost,o.PacsPort);
+                BreakerSocket(o.PacsPort+1,1024,o.PacsHost,o.PacsPort, o.Verbose);
             }
             else
             {
@@ -151,7 +167,7 @@ namespace PacsTest
             {
                 // Cut off incoming connection mid-stream
                 selfport = o.SelfPort + 1;
-                BreakerSocket(o.SelfPort,10240,"127.0.0.1",selfport);
+                BreakerSocket(o.SelfPort,10240,"127.0.0.1",selfport, o.Verbose);
             }
             else
             {
@@ -159,14 +175,31 @@ namespace PacsTest
             }
             
             var args =
-                $"-P -k \"(0010,0020)={pid}\" -k \"0008,0020={o.DateRange}\" {pacshost} {pacsport} -aet {selfname} -aec {pacsname}";
+                $"-P -k \"0008,0052=STUDY\" -k \"0010,0020={pid}\" -k \"0008,0020={o.DateRange}\" {pacshost} {pacsport} -aet {selfname} -aec {pacsname}";
             if (fetch)
             {
                 // ReSharper disable once HeapView.BoxingAllocation
                 args=$"{args} -aem {movename} --port {selfport}";
             }
-            var p = new ProcResult(fetch?"movescu":"findscu", args);
-            return p.Exitcode == 0;
+            else
+            {
+                args = $"{args} -k \"0008,1030=\"";
+            }
+            var p = new ProcResult(fetch?"movescu":"findscu", args, o.Verbose);
+            if (!String.IsNullOrWhiteSpace(p.Stdout))
+                File.WriteAllText($"{n}-out.txt",p.Stdout);
+            if (!String.IsNullOrWhiteSpace(p.Stderr))
+                File.WriteAllText($"{n}-err.txt",p.Stderr);
+
+            return n switch
+            {
+                2 => p.Stderr?.Contains($"[{o.GoodID}]", StringComparison.InvariantCultureIgnoreCase) ?? false,
+                3 => String.IsNullOrWhiteSpace(p.Stdout) && string.IsNullOrWhiteSpace(p.Stderr),
+                4 => String.IsNullOrWhiteSpace(p.Stdout) && string.IsNullOrWhiteSpace(p.Stderr),
+                9 => p.Stderr?.Contains("MoveDestinationUnknown") ?? false,
+                11 => p.Stderr?.Contains("error") ?? false,
+                _ => p.Exitcode == 0
+            };
         }
 
         static bool Test(int n,Options o)
@@ -176,7 +209,14 @@ namespace PacsTest
         
         static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<Options>(args).WithParsed(o => Test(1, o));
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o =>
+            {
+                for (var i = 1; i < 12; i++)
+                {
+                    var result = Test(i, o) ? "PASS" : "FAIL";
+                    Console.WriteLine($"Test {i}: {result}");
+                }
+            });
         }
     }
 }
